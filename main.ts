216d120257e8dc30d107a79eb5ab73c2624a2b4e
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-09-21 17:14:24 -0400

    Typescript version

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Michael B
+ * Created on: Sep 2023
+ * This program tells displays temperature on micro bit
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
+
+input.buttonIsPressed(Button.A)
