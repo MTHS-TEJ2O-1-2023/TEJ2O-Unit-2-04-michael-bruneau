5e87bd37521fe760e3f7a7c2ecd74829d6e3fb6e
michael-bruneau <144264158+michael-bruneau@users.noreply.github.com>
2023-09-21 17:07:22 -0400

    Restore '2023-09-21, 1:06:47 p.m. Add online IDE url'

--- main.ts
+++ main.ts
@@ -1,15 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Michael B
- * Created on: Sep 2023
+ * Created by: Mr. Coxall
+ * Created on: Sep 2020
  * This program ...
 */
 
-// our variable far temperature
-let temperature: number
-
-basic.clearScreen()
-basic.showIcon(IconNames.Happy)
-
-
-input.temperature()
+basic.showString('Hello, World!')
